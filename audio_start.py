6a3152d62c6da9613c5c6ba671705290202c2c40
Elieren <kir102906@gmail.com>
2023-04-15 18:51:07 +0700

    Version 0.3

--- audio_start.py
+++ audio_start.py
@@ -4,20 +4,20 @@ from tensorflow import keras
 import librosa
 from matplotlib import pyplot
 
-def get_img(wav_file_path):
+def get_mfcc(wav_file_path):
     y, sr = librosa.load(wav_file_path, offset=0, duration=30)
-    img = numpy.array(librosa.feature.img(y=y, sr=sr))
-    return img
+    mfcc = numpy.array(librosa.feature.mfcc(y=y, sr=sr))
+    return mfcc
 
-def get_hog_feature(wav_file_path):
+def get_melspectrogram(wav_file_path):
     y, sr = librosa.load(wav_file_path, offset=0, duration=30)
-    hog_feature = numpy.array(librosa.feature.hog_feature(y=y, sr=sr))
-    return hog_feature
+    melspectrogram = numpy.array(librosa.feature.melspectrogram(y=y, sr=sr))
+    return melspectrogram
 
-def get_sobel_edges_vector(wav_file_path):
+def get_chroma_vector(wav_file_path):
     y, sr = librosa.load(wav_file_path)
-    sobel_edges = numpy.array(librosa.feature.sobel_edges_stft(y=y, sr=sr))
-    return sobel_edges
+    chroma = numpy.array(librosa.feature.chroma_stft(y=y, sr=sr))
+    return chroma
 
 def get_tonnetz(wav_file_path):
     y, sr = librosa.load(wav_file_path)
@@ -25,62 +25,63 @@ def get_tonnetz(wav_file_path):
     return tonnetz
 
 def get_feature(file_path):
-    # Extracting img feature
-    img = get_img(file_path)
-    img_mean = img.mean(axis=1)
-    img_min = img.min(axis=1)
-    img_max = img.max(axis=1)
-    img_feature = numpy.concatenate( (img_mean, img_min, img_max) )
+    # Extracting MFCC feature
+    mfcc = get_mfcc(file_path)
+    mfcc_mean = mfcc.mean(axis=1)
+    mfcc_min = mfcc.min(axis=1)
+    mfcc_max = mfcc.max(axis=1)
+    mfcc_feature = numpy.concatenate( (mfcc_mean, mfcc_min, mfcc_max) )
 
     # Extracting Mel Spectrogram feature
-    hog_feature = get_hog_feature(file_path)
-    hog_feature_mean = hog_feature.mean(axis=1)
-    hog_feature_min = hog_feature.min(axis=1)
-    hog_feature_max = hog_feature.max(axis=1)
-    hog_feature_feature = numpy.concatenate( (hog_feature_mean, hog_feature_min, hog_feature_max) )
+    melspectrogram = get_melspectrogram(file_path)
+    melspectrogram_mean = melspectrogram.mean(axis=1)
+    melspectrogram_min = melspectrogram.min(axis=1)
+    melspectrogram_max = melspectrogram.max(axis=1)
+    melspectrogram_feature = numpy.concatenate( (melspectrogram_mean, melspectrogram_min, melspectrogram_max) )
 
-    # Extracting sobel_edges vector feature
-    sobel_edges = get_sobel_edges_vector(file_path)
-    sobel_edges_mean = sobel_edges.mean(axis=1)
-    sobel_edges_min = sobel_edges.min(axis=1)
-    sobel_edges_max = sobel_edges.max(axis=1)
-    sobel_edges_feature = numpy.concatenate( (sobel_edges_mean, sobel_edges_min, sobel_edges_max) )
+    # Extracting chroma vector feature
+    chroma = get_chroma_vector(file_path)
+    chroma_mean = chroma.mean(axis=1)
+    chroma_min = chroma.min(axis=1)
+    chroma_max = chroma.max(axis=1)
+    chroma_feature = numpy.concatenate( (chroma_mean, chroma_min, chroma_max) )
 
     # Extracting tonnetz feature
-    contours = get_tonnetz(file_path)
-    contours_mean = contours.mean(axis=1)
-    contours_min = contours.min(axis=1)
-    contours_max = contours.max(axis=1)
-    contours_feature = numpy.concatenate( (contours_mean, contours_min, contours_max) ) 
+    tntz = get_tonnetz(file_path)
+    tntz_mean = tntz.mean(axis=1)
+    tntz_min = tntz.min(axis=1)
+    tntz_max = tntz.max(axis=1)
+    tntz_feature = numpy.concatenate( (tntz_mean, tntz_min, tntz_max) ) 
 
-    feature = numpy.concatenate( (sobel_edges_feature, hog_feature_feature, img_feature, contours_feature) )
+    feature = numpy.concatenate( (chroma_feature, melspectrogram_feature, mfcc_feature, tntz_feature) )
     return feature
 
 #---------------------------------------------------------------------------------#
 
-object_1 = ['Rock','Phonk','Synthwave','Jazz','EDM','Metal','Nightcore','Dubstep','Score','Frenchcore','Uptempo']
-print(len(object_1))
+genres_1 = ['Rock','Phonk','Synthwave','Jazz','EDM','Metal','Nightcore','Dubstep','Score','Frenchcore','Uptempo']
+print(len(genres_1))
 features = []
 labels = []
 
-with open('C:\\Users\\kazan\\Videos\\git\\music_genres\\info\\genres_final.txt') as f:
-    object_list = f.read().splitlines()
-
 # Путь к папке с аудиофайлами
-pictures_folder = 'C:\\Users\\kazan\\Videos\\git\\music_genres\\info\\music_WAV'
+audio_folder = 'C:\\Users\\kazan\\Videos\\git\\music_genres\\info\\music_WAV'
 
 # Список файлов в папке
-pictures_files = os.listdir(pictures_folder)
+audio_files = os.listdir(audio_folder)
 
 # Инициализация списков признаков и меток жанров
 
 # Перебор каждого файла в папке
-for a, pictures_file in enumerate(pictures_files):
-    file_path = 'C:\\Users\\kazan\\Videos\\git\\music_genres\\info\\music_WAV\\' + pictures_file
-
-    features.append(get_feature(file_path))
-    labels.append(object_1.index(object_list[a]))
-    print(pictures_file, object_list[a], object_1.index(object_list[a]))
+for genre_folder in os.listdir(audio_folder):
+    genre_path = os.path.join(audio_folder, genre_folder)
+    if os.path.isdir(genre_path) and any(substring in genre_folder for substring in genres_1):
+        # Перебор каждого WAV файла в папке-жанре
+        for audio_file in os.listdir(genre_path):
+            if audio_file.endswith('.wav'):
+                # Добавление признаков и метки жанра в соответствующие списки
+                genre = [substring for substring in genres_1 if substring in genre_folder][0]
+                features.append(get_feature(os.path.join(genre_path, audio_file)))
+                labels.append(genres_1.index(genre))
 
 #-------------------------------------------------------------------------#
 
@@ -124,47 +125,47 @@ file_path = "C:\\Users\\kazan\\Desktop\\1.mp4.wav"
 feature = get_feature(file_path)
 y = model.predict(feature.reshape(1,498))
 ind = numpy.argmax(y)
-print(object_1[ind], '=> Phonk (MUKBANG)')
+print(genres_1[ind], '=> Phonk (MUKBANG)')
 
 file_path = "C:\\Users\\kazan\\Desktop\\2.wav"
 feature = get_feature(file_path)
 y = model.predict(feature.reshape(1,498))
 ind = numpy.argmax(y)
-print(object_1[ind], '=> ?El Tigro')
+print(genres_1[ind], '=> ?El Tigro')
 
 file_path = "C:\\Users\\kazan\\Desktop\\3.mp4.wav"
 feature = get_feature(file_path)
 y = model.predict(feature.reshape(1,498))
 ind = numpy.argmax(y)
-print(object_1[ind], '=> EDM (Nana)')
+print(genres_1[ind], '=> EDM (Nana)')
 
 file_path = "C:\\Users\\kazan\\Desktop\\1.wav"
 feature = get_feature(file_path)
 y = model.predict(feature.reshape(1,498))
 ind = numpy.argmax(y)
-print(object_1[ind], '=> ? (Been Good To Know Ya)')
+print(genres_1[ind], '=> ? (Been Good To Know Ya)')
 
 file_path = "C:\\Users\\kazan\\Desktop\\2.mp4.wav"
 feature = get_feature(file_path)
 y = model.predict(feature.reshape(1,498))
 ind = numpy.argmax(y)
-print(object_1[ind], '=> EDM (Zenith)')
+print(genres_1[ind], '=> EDM (Zenith)')
 
 '''
 example_file = "C:\\Users\\kazan\\Videos\\git\\music_genres\\info\\music_WAV\\Cyberpunk_2077_-_The_Ballad_of_Buck_Ravers_by_SAMURAI_Refused_69859526(rock).wav"
-img = get_img(example_file)
-pyplot.imshow(img, interpolation='nearest', aspect='auto')
+mfcc = get_mfcc(example_file)
+pyplot.imshow(mfcc, interpolation='nearest', aspect='auto')
 pyplot.show()
 
-hog_feature = get_hog_feature(example_file)
-pyplot.imshow(hog_feature, interpolation='nearest', aspect='auto')
+melspectrogram = get_melspectrogram(example_file)
+pyplot.imshow(melspectrogram, interpolation='nearest', aspect='auto')
 pyplot.show()
 
-sobel_edges = get_sobel_edges_vector(example_file)
-pyplot.imshow(sobel_edges, interpolation='nearest', aspect='auto')
+chroma = get_chroma_vector(example_file)
+pyplot.imshow(chroma, interpolation='nearest', aspect='auto')
 pyplot.show()
 
-contours = get_tonnetz(example_file)
-pyplot.imshow(contours , interpolation='nearest', aspect='auto')
+tntz = get_tonnetz(example_file)
+pyplot.imshow(tntz , interpolation='nearest', aspect='auto')
 pyplot.show()
 '''
